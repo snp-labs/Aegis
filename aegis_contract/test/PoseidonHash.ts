bef95abac65c8628b976b133bd1e20abf001f0a3
Han-16 <haanbk16@gmail.com>
2024-12-05 22:16:21 +0900

    refactor: restructure hash function to handle only two elements

--- aegis_contract/test/PoseidonHash.ts
+++ aegis_contract/test/PoseidonHash.ts
@@ -26,31 +26,35 @@ describe("Poseidon Hasher", () => {
     });
 
     it.only("should return the correct hash [PoseidonHashLib]", async() => {
-        const input = ethers.toBigInt("13426023570019593411025966325317987258055918306904482401598026097290337983680");
-        const digest = await poseidonHasher.hashPoseidon([input]);
-        const digest_expected = ethers.toBigInt("919503531753078779876520987165713340315120209391397874144021196597115396594");
+        const input1 = ethers.toBigInt("19933809757920020759955474368277437557688215414721001481155398693244893563064");
+        const input2 = ethers.toBigInt("8388539055774977664058499614800672349209703707994793864989619367039077778209");
+        const digest = await poseidonHasher.hashPoseidonTwoToOne(input1, input2);
+        const digest_expected = ethers.toBigInt("4366155491452329913680757329953861545146738755768024031653723098266737219231");
         console.log("digest: ", digest);
         console.log("digest_expected: ", digest_expected);
         expect(digest).to.be.equal(digest_expected);
     });
 
     it("should return the correct hash [Keccack]", async() => {
-        const input = ethers.toBigInt("7524178265202101577084604334552339139954807715757712981859874388306919542679");
-        const digest= await poseidonHasher.hashKeccak([input]);
+        const input1 = ethers.toBigInt("19933809757920020759955474368277437557688215414721001481155398693244893563064");
+        const input2 = ethers.toBigInt("8388539055774977664058499614800672349209703707994793864989619367039077778209");
+        const digest= await poseidonHasher.hashKeccak(input1, input2);
         console.log(digest);
     });
 
     it("should return the correct hash [MiMC7]", async() => {
-        const input = ethers.toBigInt("7524178265202101577084604334552339139954807715757712981859874388306919542679");
-        const digest= await poseidonHasher.hashMiMC7([input]);
+        const input1 = ethers.toBigInt("19933809757920020759955474368277437557688215414721001481155398693244893563064");
+        const input2 = ethers.toBigInt("8388539055774977664058499614800672349209703707994793864989619367039077778209");
+        const digest= await poseidonHasher.hashMiMC7(input1, input2);
         console.log(digest);
     });
 
     it.only("should measure gas cost : [PoseidonLib]", async() => {
-        const input = ethers.toBigInt("11235");
-        const PoseidonHashLibGasCost = await poseidonHasher.hashPoseidon.estimateGas([input]);
-        const MiMC7GasCost = await poseidonHasher.hashMiMC7.estimateGas([input]);
-        const KeccakGasCost = await poseidonHasher.hashKeccak.estimateGas([input]);
+        const input1 = ethers.toBigInt("19933809757920020759955474368277437557688215414721001481155398693244893563064");
+        const input2 = ethers.toBigInt("8388539055774977664058499614800672349209703707994793864989619367039077778209");
+        const PoseidonHashLibGasCost = await poseidonHasher.hashPoseidonTwoToOne.estimateGas(input1, input2);
+        const MiMC7GasCost = await poseidonHasher.hashMiMC7.estimateGas(input1, input2);
+        const KeccakGasCost = await poseidonHasher.hashKeccak.estimateGas(input1, input2);
         console.log("[MiMC7]: ", MiMC7GasCost.toString());
         console.log("[Keccak]: ", KeccakGasCost.toString());
         console.log("[Poseidon]: ", PoseidonHashLibGasCost.toString());
